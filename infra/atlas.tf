1769321bfe81c2e947eb7cf7f6fa897fefbed607
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2024-10-01 09:15:03 -0300

    feat: mongodb infra settings

--- infra/atlas.tf
+++ infra/atlas.tf
@@ -40,20 +40,6 @@ resource "mongodbatlas_cluster" "atlas-cluster" {
   project_id = mongodbatlas_project.aws_atlas.id
   name = "${var.atlas_project_name}-cluster"
   cluster_type = "REPLICASET"
-  replication_specs {
-    num_shards = 1
-    regions_config {
-      region_name     = "US_EAST_1"
-      electable_nodes = 3
-      priority        = 7
-      read_only_nodes = 0
-    }
-  }
-  cloud_backup = true
-  auto_scaling_disk_gb_enabled = true
-  mongo_db_major_version       = "7.0"
-
-  # Provider Settings "block"
   provider_name               = "AWS"
   provider_instance_size_name = "M0"
 }
