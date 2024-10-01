4c6fdb7127db2adbc5ca3b505acebedd4d9a5333
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2024-10-01 09:00:21 -0300

    feat: mongodb infra settings

--- infra/atlas.tf
+++ infra/atlas.tf
@@ -36,36 +36,35 @@ resource "mongodbatlas_project_ip_access_list" "cidr_block" {
   comment    = "cidr block for AWS VPC"
 }
 
-resource "mongodbatlas_advanced_cluster" "atlas-cluster" {
+resource "mongodbatlas_cluster" "atlas-cluster" {
   project_id = mongodbatlas_project.aws_atlas.id
   name = "${var.atlas_project_name}-cluster"
   cluster_type = "REPLICASET"
-  backup_enabled = true
-  mongo_db_major_version =  6.0
   replication_specs {
-    region_configs {
-      electable_specs {
-        instance_size = "M0"
-        node_count    = 3
-      }
-      analytics_specs {
-        instance_size = "M0"
-        node_count    = 1
-      }
-      priority      = 7
-      provider_name = "AWS"
-      region_name   = var.atlas_region
+    num_shards = 1
+    regions_config {
+      region_name     = "US_EAST_1"
+      electable_nodes = 3
+      priority        = 7
+      read_only_nodes = 0
     }
   }
+  cloud_backup = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "7.0"
+
+  # Provider Settings "block"
+  provider_name               = "AWS"
+  provider_instance_size_name = "M0"
 }
 
 data "mongodbatlas_advanced_cluster" "atlas-cluser" {
   project_id = mongodbatlas_project.aws_atlas.id
-  name       = mongodbatlas_advanced_cluster.atlas-cluster.name
+  name       = mongodbatlas_cluster.atlas-cluster.name
   depends_on = [mongodbatlas_privatelink_endpoint_service.atlaseplink]
 }
 
-output "atlas_cluster_connection_string" { value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv }
+output "atlas_cluster_connection_string" { value = mongodbatlas_cluster.atlas-cluster.connection_strings.0.standard_srv }
 output "ip_access_list"    { value = mongodbatlas_project_ip_access_list.cidr_block.cidr_block }
 output "project_name"      { value = mongodbatlas_project.aws_atlas.name }
 output "username"          { value = mongodbatlas_database_user.db-user.username }
